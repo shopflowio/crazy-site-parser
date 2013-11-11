1b95e8ebfb36760c775c0d78a7508765931bd68e
Torey Heinz <toreyheinz@gmail.com>
2013-11-11 15:17:29 -0500

    made script into class, to test with irb

--- scrapper.rb
+++ scrapper.rb
@@ -1,44 +1,47 @@
 #rvm 1.8.7
-require 'rubygems'
-require 'nokogiri'
-require 'awesome_print'
-require 'fileutils'
-require 'sanitize'
-require 'tidy_ffi'
+class Scrapper
 
-def parse_page(page)
-  doc = Nokogiri::HTML(File.open page)
-  {}.tap do |data|
-    data[:url]              = "http://www.delloro.com/#{page}"
-    data[:title]            = doc.at_css('title').text if doc.at_css('title')
-    data[:meta_description] = doc.at_css('meta[name="DESCRIPTION"]')['content'] if doc.at_css('meta[name="DESCRIPTION"]')
-    data[:content]          = doc.at_css('.maincol').inner_html if doc.at_css('.maincol')
+  require 'rubygems'
+  require 'nokogiri'
+  require 'awesome_print'
+  require 'fileutils'
+  require 'sanitize'
+  require 'tidy_ffi'
+
+  def self.parse_page(page)
+    doc = Nokogiri::HTML(File.open page)
+    {}.tap do |data|
+      data[:url]              = "http://www.delloro.com/#{page}"
+      data[:title]            = doc.at_css('title').text if doc.at_css('title')
+      data[:meta_description] = doc.at_css('meta[name="DESCRIPTION"]')['content'] if doc.at_css('meta[name="DESCRIPTION"]')
+      data[:content]          = doc.at_css('.maincol').inner_html if doc.at_css('.maincol')
+    end
   end
-end
 
-def sanitize_html(html)
-  Sanitize.clean(html, Sanitize::Config::RELAXED.merge(:remove_contents => true))
-end
+  def self.sanitize_html(html)
+    Sanitize.clean(html, Sanitize::Config::RELAXED.merge(:remove_contents => true))
+  end
 
-def tidy_html(html)
-  TidyFFI::Tidy.with_options(
-    :indent           => 'yes',
-    :tidy_mark        => false,
-    :char_encoding    => 'utf8',
-    :doctype          => 'omit',
-    :bare             => true,
-    :clean            => true,
-    :break_before_br  => true,
-    :drop_empty_paras => true
-    ).new(html).clean
-end
+  def self.tidy_html(html)
+    TidyFFI::Tidy.with_options(
+      :indent           => 'yes',
+      :tidy_mark        => false,
+      :char_encoding    => 'utf8',
+      :doctype          => 'omit',
+      :bare             => true,
+      :clean            => true,
+      :break_before_br  => true,
+      :drop_empty_paras => true
+      ).new(html).clean
+  end
 
-def clean_html(html)
-  tidy_html sanitize_html(html)
-end
+  def self.clean_html(html)
+    tidy_html sanitize_html(html)
+  end
 
-Dir.chdir('www.broadviewproduct.com')
-pages = Dir['**/*.htm*']
+end
+#Dir.chdir('www.broadviewproduct.com')
+#puts pages = Dir['**/*.htm*']
 
 # pages.each do |page|
 #   new_path = "../wp.delloro/#{page}"
