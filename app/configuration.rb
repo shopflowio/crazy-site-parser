c6c5b9040487fec42093357c9d79f95bc0813575
jim <jim@crunchbang.(none)>
2013-11-15 13:37:51 -0500

    cleaned up and commented

--- app/configuration.rb
+++ app/configuration.rb
@@ -1,12 +1,24 @@
+#- Something I was toying with for a minute, but I'm not going to think
+#  about until after I revamp and integrate the nokogiri logic some more.
+
+#- There is a Global class for global, hard-coded configurations.
+#  And then we have database.yml.
+
+#  database.yml is a user-level configuration, and I may develop another
+#  user-level config system for scraping. But I'm not there yet...
+
+#  This class wouldn't be another place to stuff configuration settings,
+#  but an object-oriented way of dealing with configurations. If need be...
+
 class Configuration
-  require './modeller'
-
-  def initialize(options = {})
-    for option in options
-      raise "#{option} takes a Hash" unless option.instance_of? Hash
-    end
-
-    
-  end
+#  require './modeller'
+#
+#  def initialize(options = {})
+#    for option in options
+#      raise "#{option} takes a Hash" unless option.instance_of? Hash
+#    end
+#
+#    
+#  end
 
 end
