6479dacd390b45048ddeb834d1626a4def1cff31
jim <jim@crunchbang.(none)>
2013-11-16 14:27:43 -0500

    wrote test for seperate_elements, fixed logic in yml
    I discovered that it's actually to_html I want in the yml,
    not inner_html. Though that was an issue with user-supplied
    code, not app code. Test passes
    
    On that note, eventually there should be some rake tasks to
    help the user debug the code in their yaml. Though that is
    definitely in the icebox.

--- test/page_filter_test.rb
+++ test/page_filter_test.rb
@@ -7,19 +7,22 @@ class PageFilterTest < MiniTest::Unit::TestCase
   def setup
     @config = Configuration.new('test/config/nokogiri.yml')
     @path   = 'test/bpd/aboutus.htm'
+    @pf = PageFilter.new(path: @path, config: @config)
   end
 
   def test_initialize
-    pf = PageFilter.new(path: @path, config: @config)
-
-    assert !pf.content.nil?
+    assert !@pf.content.nil?
   end
 
   def test_parse_content
-    pf = PageFilter.new(path: @path, config: @config)
-    
-    assert pf.parse_content.instance_of? String
-    assert !pf.parse_content.empty?
+    assert @pf.parse_content.instance_of? String
+    assert !@pf.parse_content.empty?
+  end
+
+  def test_seperate_elements
+    regexp = Regexp.new(@config.seperator_string)
+
+    assert regexp.match @pf.parse_content
   end
 
 end
